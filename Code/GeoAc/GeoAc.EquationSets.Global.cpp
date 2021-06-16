0905b2d8524664ab02bf5cb2107da73fa20a2e32
Ravi Shankar <ravishankar@u.boisestate.edu>
2021-06-15 18:36:08 -0600

    Moving codegen call down

--- Code/GeoAc/GeoAc.EquationSets.Global.cpp
+++ Code/GeoAc/GeoAc.EquationSets.Global.cpp
@@ -239,8 +239,8 @@ void GeoAc_UpdateSources(double ray_length, double* current_values, GeoAc_Source
     //sources.u = u(r,theta,phi, spl.Windu_Spline);
     
     //sources.dc[0] = c_diff(r,theta,phi,0,spl.Temp_Spline);
+    //sources.dw[0] = w_diff(r,theta,phi,0);
     #include "codegen.c"
-    sources.dw[0] = w_diff(r,theta,phi,0);
     sources.dv[0] = v_diff(r,theta,phi,0,spl.Windv_Spline);
     sources.du[0] = u_diff(r,theta,phi,0,spl.Windu_Spline);
     
